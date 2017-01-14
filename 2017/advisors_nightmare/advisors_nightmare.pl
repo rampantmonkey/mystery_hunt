56ae02e0bf1f27bebca964b35ac0dfd0ddfdb93d
Scott Steele <scott.steele@distilnetworks.com>
2017-01-14 16:02:56 -0500

    Add all course conditions except Sudoku sandwich

--- 2017/advisors_nightmare/advisors_nightmare.pl
+++ 2017/advisors_nightmare/advisors_nightmare.pl
@@ -4,6 +4,20 @@ all_members([H], L2) :- member(H, L2).
 all_members([H|T],L2) :- member(H, L2), all_members(T, L2).
 
 
+ % delete(L,E,LN) is true if a list LN is identical to a list L
+ % after removing all occurences of an element E.
+ delete([H|T],H,TN) :- delete(T,H,TN).
+ delete([H|T],E,[H|TN]) :- \+ H=E, delete(T,E,TN).
+ delete([],_,[]).
+
+ % unique(L,LN) is true if a list LN is identical to a list L
+ % without all repeated elements.
+ unique([H|T],[H|TU]) :- delete(T,E,TN), unique(TN,TU).
+ unique([],[]).
+
+all_diff(List) :-
+  \+ (select(X, List, R), memberchk(X, R)).
+
 solve(Schedule) :-
   Schedule = [C01_1, C01_2, C01_3, C02_1, C02_2, C02_3, C03_1, C03_2, C03_3, C04_1, C04_2, C04_3, C05_1, C05_2, C05_3, C06_1, C06_2, C06_3, C07_1, C07_2, C07_3, C08_1, C08_2, C08_3, C09_1, C09_2, C09_3, C10_1, C10_2, C10_3, C11_1, C11_2, C11_3, C12_1, C12_2, C12_3],
 
@@ -139,6 +153,198 @@ solve(Schedule) :-
   C12_2_slot,
   C12_2_slot <
   C12_3_slot,
+
+  member([e102,   _, E102_trimester],   Schedule),
+  member([e115,   _, E115_trimester],   Schedule),
+  member([e128,   _, E128_trimester],   Schedule),
+  member([e145_1, _, E145_1_trimester], Schedule),
+  member([e145_2, _, E145_2_trimester], Schedule),
+  member([e156,   _, E156_trimester],   Schedule),
+  member([e176,   _, E176_trimester],   Schedule),
+  member([e180,   _, E180_trimester],   Schedule),
+  member([e199,   _, E199_trimester],   Schedule),
+  member([e205,   _, E205_trimester],   Schedule),
+  member([e234,   _, E234_trimester],   Schedule),
+  member([e253,   _, E253_trimester],   Schedule),
+  member([e265,   _, E265_trimester],   Schedule),
+  member([e272,   _, E272_trimester],   Schedule),
+  member([e289,   _, E289_trimester],   Schedule),
+  member([e308,   _, E308_trimester],   Schedule),
+  member([e332,   _, E332_trimester],   Schedule),
+  member([e343,   _, E343_trimester],   Schedule),
+  member([e344,   _, E344_trimester],   Schedule),
+  member([e361,   _, E361_trimester],   Schedule),
+  member([e373,   _, E373_trimester],   Schedule),
+  member([e390,   _, E390_trimester],   Schedule),
+  member([e397,   _, E397_trimester],   Schedule),
+  member([e402,   _, E402_trimester],   Schedule),
+  member([e421,   _, E421_trimester],   Schedule),
+  member([e426,   _, E426_trimester],   Schedule),
+  member([e428,   _, E428_trimester],   Schedule),
+  member([e438,   _, E438_trimester],   Schedule),
+  member([e440,   _, E440_trimester],   Schedule),
+  member([e468,   _, E468_trimester],   Schedule),
+  member([e474,   _, E474_trimester],   Schedule),
+  member([e475,   _, E475_trimester],   Schedule),
+  member([e504,   _, E504_trimester],   Schedule),
+  member([e512,   _, E512_trimester],   Schedule),
+  member([e516,   _, E516_trimester],   Schedule),
+  member([e530,   _, E530_trimester],   Schedule),
+
+  % ENGM 102
+  member(E102_trimester - E102_trimester, [-1, 1]),
+  member(E102_trimester, [1, 4, 7, 10]),
+
+  % ENGM 115
+  E115_trimester =\= E265_trimester,
+
+  % ENGM 128
+  member(E128_trimester, [1, 2, 4, 5, 7, 8, 10, 11]),
+
+  % Engm 145
+  E145_1_trimester > E390_trimester,
+  E145_2_trimester > E145_1_trimester,
+  member(E145_1_trimester, [1, 2, 4, 5, 7, 8, 10, 11]),
+  member(E145_2_trimester, [1, 2, 4, 5, 7, 8, 10, 11]),
+
+  % Engm 156
+  member(E156_trimester - E512_trimester, [-1, 1]),
+
+  % Engm 176
+  E176_trimester =:= E438_trimester + 1,
+
+  % Engm 180
+  E180_trimester > E234_trimester,
+
+  % e199
+  E199_trimester > E344_trimester,
+  member(E199_trimester, [1, 4, 7, 10]),
+
+  % e205
+  E205_trimester =:= E128_trimester,
+  member(E205_trimester, [1, 4, 7, 10]),
+
+  % e234
+  % No conditions
+
+  % e253
+  E253_trimester > E530_trimester,
+  member(E253_trimester, [2, 5, 8, 11]),
+
+  % e265
+  member(E265_trimester - E102_trimester, [-1, 1]),
+
+  % e272
+  E272_trimester > E512_trimester,
+  member(E272_trimester, [1, 4, 7, 10]),
+
+  % e289
+  % Must be taken immediately before and after a sudoku seminar
+
+  % e308
+  E308_trimester =:= E332_trimester,
+
+  % e332
+  % No conditions
+
+  % e343
+  E343_trimester > E199_trimester,
+  member(E343_trimester, [3, 6, 9, 12]),
+
+  % e344
+  E344_trimester > E145_2_trimester,
+  member(E344_trimester, [2, 5, 8, 11]),
+
+  % e361
+  E361_trimester >= E390_trimester + 9,
+
+  % e373
+  E373_trimester > 1,
+
+  % e390
+  E390_trimester =:= E373_trimester,
+
+  % e397
+  E397_trimester > E516_trimester,
+  member(E397_trimester, [1, 4, 7, 10]),
+
+  % e402
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  all_diff([ E402_trimester
+           , E421_trimester
+           , E426_trimester
+           , E428_trimester
+           , E438_trimester
+           , E440_trimester
+           , E468_trimester
+           , E474_trimester
+           , E475_trimester
+           ]),
+
+  % e421
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  % 426 must be taken during same academic year
+  div(E421_trimester, 3) =:= div(E426_trimester, 3),
+
+  % e426
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+
+
+  % e428
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  E428_trimester > E402_trimester,
+
+
+  % e438
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+
+
+  % e440
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  member(E440_trimester, [2, 5, 8, 11]),
+
+
+  % e468
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  E468_trimester =:= E438_trimester + 5,
+
+
+  % e474
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  member(E474_trimester, [1, 2, 4, 5, 7, 8, 10, 11]),
+
+
+  % e475
+  % Sudoku seminar. At most one Sudoku seminar per trimester.
+  member(E475_trimester, [3, 6, 9, 12]),
+
+  % Kunundrum Klasses:
+  all_members(
+    [3, 6, 9, 12],
+    [E504_trimester,
+     E512_trimester,
+     E516_trimester,
+     E530_trimester]
+  ),
+
+  % e504
+  % Kunundrum Klass. At most one Kunundrum Klass per trimester.
+  % member(E504_trimester, [3, 6, 9, 12]),
+
+  % e512
+  % Kunundrum Klass. At most one Kunundrum Klass per trimester.
+  % member(E512_trimester, [3, 6, 9, 12]),
+
+
+  % e516
+  % Kunundrum Klass. At most one Kunundrum Klass per trimester.
+  % member(E516_trimester, [3, 6, 9, 12]),
+
+
+  % e530
+  % Kunundrum Klass. At most one Kunundrum Klass per trimester.
+  % member(E530_trimester, [3, 6, 9, 12]),
+
 
   all_members(
   [e102, e115, e128, e145_1, e145_2, e156, e176, e180, e199, e205, e234, e253, e265, e272, e289, e308, e332, e343, e344, e361, e373, e390, e397, e402, e421, e426, e428, e438, e440, e468, e474, e475, e504, e512, e516, e530],
